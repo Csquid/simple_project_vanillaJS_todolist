5016e8d7f0a79bb67cce4c6ca5e5f38f22cb0b4c
jjy_codemonkey <36003645+Csquid@users.noreply.github.com>
2021-03-31 18:24:36 +0900

    test

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 window.onload = () => {
   const itemContainerElements = document.querySelectorAll(".item-container");
+  const todoListDatas = [
+    {id: 0, todo: 'Study React Library'},
+    {id: 1, todo: 'Read the a Book'},
+    {id: 2, todo: 'Go to the School'}
+  ];
+
+
+  //Read
+  const itemContentElement   = document.querySelectorAll('.item-content');
+  const itemContainerElement = document.createElement('div');
+  const todoCheckBoxElement  = document.createElement('input');
+  const todoTextElement      = document.createElement('input');
+  const updateButtonElement  = document.createElement('input');
+  const deleteButtonElement  = document.createElement('input');
+
+  itemContainerElement.className = 'item-container';
+
+  todoCheckBoxElement.className = 'todo_checkbox';
+  todoCheckBoxElement.type  = "checkbox";
+  
+  todoTextElement.type = 'text';
+  todoTextElement.value = '1234';
+  todoTextElement.readOnly = 'readOnly';
+  
+  updateButtonElement.className = 'update';
+  updateButtonElement.type = 'button';
+  updateButtonElement.value = '🦜';
+
+  deleteButtonElement.className = 'delete';
+  deleteButtonElement.type = 'button';
+  deleteButtonElement.value = 'x';
+
+  itemContainerElement.appendChild(todoCheckBoxElement);
+  itemContainerElement.appendChild(todoTextElement);
+  itemContainerElement.appendChild(updateButtonElement);
+  itemContainerElement.appendChild(deleteButtonElement);
+  
+  itemContentElement[0].appendChild(itemContainerElement);
+  //Create
+
+  //Update
+
+  //Delete
 
   for(let i = 0; i < itemContainerElements.length; i++) {
     const todoElementsChildren = itemContainerElements[i].children;
@@ -14,22 +57,20 @@ window.onload = () => {
     for(let j = 0; j < todoObjectKeys.length; j++) {
       todoElementOBJ[todoObjectKeys[j]] = todoElementsChildren[j];
     }
-    // firstElementChild 첫번째 자식을 찾는다
-    // nextElementSibling 다음 형제를 찾는다
 
     todoElementOBJ.checkbox.addEventListener('click', (e) => {
-      if(todoElementOBJ.text.className === "") {
-        todoElementOBJ.text.className = "check";
-        todoElementOBJ.delete.style["display"] = "inline-block";
+      if(todoElementOBJ.text.className === '') {
+        todoElementOBJ.text.className = 'check';
+        todoElementOBJ.delete.style["display"] = 'inline-block';
         todoElementOBJ.update.style['display'] = 'none';
       } else {
-        todoElementOBJ.text.className = "";
-        todoElementOBJ.delete.style["display"] = "none";
+        todoElementOBJ.text.className = '';
+        todoElementOBJ.delete.style['display'] = 'none';
       }
     });
     
     itemContainerElements[i].addEventListener('mouseover', (e) => {
-      /* 체크박스에 체크가 되었다면 이벤트 무시*/
+      /* 체크박스에 체크가 되었다면 이벤트 무시 */
 
       if(todoElementOBJ.text.className === 'check') {
         todoElementOBJ.update.style['display'] = 'none';
@@ -40,8 +81,8 @@ window.onload = () => {
     });
 
     itemContainerElements[i].addEventListener('mouseout', () => {
-      // if(todoElementOBJ.text.className === 'check')
-      //   return;
+      if(todoElementOBJ.text.className === 'check')
+        return;
 
       todoElementOBJ.update.style['display'] = 'none';
     });

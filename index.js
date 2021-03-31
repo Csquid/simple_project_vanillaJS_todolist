281c57303b8ed989811ae9bb8bb908b7e058f818
jjy_codemonkey <36003645+Csquid@users.noreply.github.com>
2021-03-31 22:42:53 +0900

    Bug Fix , Create LocalStorage
    Bug Fix 
      Modify if margin becomes weird when you dynamically insert todolist in JavaScript [JavaScript에서 동적으로 목록을 삽입할 때 margin이 이상해지는 경우 수정]
      It's because of the font size. [원인은 폰트 사이즈 였다.]
      So The appropriate part was given a font size of 0 and the margin was processed overall. [그래서 해당 영역의 글꼴 크기를 0을 주었고 전체적으로 margin이 처리되었습니다.]
    + [ Create LocalStorage  + create read part ]

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
 window.onload = () => {
-  const itemContainerElements = document.querySelectorAll(".item-container");
+  let itemContainerElements = document.querySelectorAll(".item-container");
+  const TodoLocalStorage = "todos";
   const todoListDatas = [
     {id: 0, todo: 'Study React Library'},
-    {id: 1, todo: 'Read the a Book'},
-    {id: 2, todo: 'Go to the School'}
+    {id: 1, todo: 'Read an Book'},
+    {id: 2, todo: 'Go to School'}
   ];
-
+  
+  /* Local Storage 에서 데이터 셋팅하기 */
+  localStorage.setItem(TodoLocalStorage, JSON.stringify(todoListDatas));
+  // localStorage.setItem(TodoLocalStorage, JSON.stringify([{id:2, todo: 'test2'}]));
+  /* Local Storage 에서 데이터 꺼내오기 */
+  let todos = localStorage.getItem(TodoLocalStorage);
+  const parsedTodos = JSON.parse(todos);
 
   //Read
-  const itemContentElement   = document.querySelectorAll('.item-content');
-  const itemContainerElement = document.createElement('div');
-  const todoCheckBoxElement  = document.createElement('input');
-  const todoTextElement      = document.createElement('input');
-  const updateButtonElement  = document.createElement('input');
-  const deleteButtonElement  = document.createElement('input');
-
-  itemContainerElement.className = 'item-container';
-
-  todoCheckBoxElement.className = 'todo_checkbox';
-  todoCheckBoxElement.type  = "checkbox";
   
-  todoTextElement.type = 'text';
-  todoTextElement.value = '1234';
-  todoTextElement.readOnly = 'readOnly';
+  for(let i = 0; i < parsedTodos.length; i++) {
+    const itemContentElement   = document.querySelectorAll('.item-content')[0];
+    const itemContainerElement = document.createElement('div');
+    const todoCheckBoxElement  = document.createElement('input');
+    const todoTextElement      = document.createElement('input');
+    const updateButtonElement  = document.createElement('input');
+    const deleteButtonElement  = document.createElement('input');
   
-  updateButtonElement.className = 'update';
-  updateButtonElement.type = 'button';
-  updateButtonElement.value = '🦜';
-
-  deleteButtonElement.className = 'delete';
-  deleteButtonElement.type = 'button';
-  deleteButtonElement.value = 'x';
-
-  itemContainerElement.appendChild(todoCheckBoxElement);
-  itemContainerElement.appendChild(todoTextElement);
-  itemContainerElement.appendChild(updateButtonElement);
-  itemContainerElement.appendChild(deleteButtonElement);
+    itemContainerElement.className = 'item-container';
   
-  itemContentElement[0].appendChild(itemContainerElement);
+    todoCheckBoxElement.className = 'todo_checkbox';
+    todoCheckBoxElement.type  = "checkbox";
+    
+    todoTextElement.type = 'text';
+    todoTextElement.defaultValue = parsedTodos[i].todo;
+    todoTextElement.readOnly = 'readOnly';
+    
+    updateButtonElement.className = 'update';
+    updateButtonElement.type = 'button';
+    updateButtonElement.value = '🦜';
+  
+    deleteButtonElement.className = 'delete';
+    deleteButtonElement.type = 'button';
+    deleteButtonElement.value = 'x';
+  
+    itemContainerElement.appendChild(todoCheckBoxElement);
+    itemContainerElement.appendChild(todoTextElement);
+    itemContainerElement.appendChild(updateButtonElement);
+    itemContainerElement.appendChild(deleteButtonElement);
+    
+    itemContentElement.appendChild(itemContainerElement);
+  }
   //Create
 
   //Update
 
   //Delete
+
+  itemContainerElements = document.querySelectorAll(".item-container");
 
   for(let i = 0; i < itemContainerElements.length; i++) {
     const todoElementsChildren = itemContainerElements[i].children;
@@ -62,7 +74,7 @@ window.onload = () => {
       if(todoElementOBJ.text.className === '') {
         todoElementOBJ.text.className = 'check';
         todoElementOBJ.delete.style["display"] = 'inline-block';
-        // todoElementOBJ.update.style['display'] = 'none';
+        todoElementOBJ.update.style['display'] = 'none';
       } else {
         todoElementOBJ.text.className = '';
         todoElementOBJ.delete.style['display'] = 'none';
@@ -73,18 +85,18 @@ window.onload = () => {
       /* 체크박스에 체크가 되었다면 이벤트 무시 */
 
       if(todoElementOBJ.text.className === 'check') {
-        // todoElementOBJ.update.style['display'] = 'none';
+        todoElementOBJ.update.style['display'] = 'none';
         return;
       }
 
-      // todoElementOBJ.update.style['display'] = 'inline-block';
+      todoElementOBJ.update.style['display'] = 'inline-block';
     });
 
     itemContainerElements[i].addEventListener('mouseout', () => {
       if(todoElementOBJ.text.className === 'check')
         return;
 
-      // todoElementOBJ.update.style['display'] = 'none';
+      todoElementOBJ.update.style['display'] = 'none';
     });
 
   }
